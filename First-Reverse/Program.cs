a47db7441392d2553c9cc4e3bcb682951cbbf162
Roger Leblanc <roger@rogerleblanc.net>
2019-10-08 01:29:13 -0400

    add first reverse

---
+++ First-Reverse/Program.cs
@@ -0,0 +1,22 @@
+﻿//https://coderbyte.com/information/First%20Reverse
+
+using System;
+
+class MainClass
+{
+
+    public static string FirstReverse(string str)
+    {
+
+        // code goes here  
+        return str;
+
+    }
+
+    static void Main()
+    {
+        // keep this function call here
+        Console.WriteLine(FirstReverse(Console.ReadLine()));
+    }
+
+}
