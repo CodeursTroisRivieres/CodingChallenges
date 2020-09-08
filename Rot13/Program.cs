a4d82f12e418902bac0f3f3702e70c56b6a7549b
Roger Leblanc <roger@rogerleblanc.net>
2020-09-07 21:15:00 -0400

    Add Rot13

---
+++ Rot13/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Rot13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Rot13("Te1235@£¢{53564654`<st"));
+            Console.WriteLine(Kata.Rot13("test"));
+        }
+    }
+}
---
+++ Rot13/Rot13.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+public class Kata
+{
+    private static readonly ReadOnlyCollection<char> LowerCaseLetters;
+    private static readonly ReadOnlyCollection<char> UpperCaseLetters;
+
+    static Kata()
+    {
+        LowerCaseLetters = new ReadOnlyCollection<char>(Enumerable
+            .Range('a', 26)
+            .Select(i => (char)i)
+            .ToList());
+
+        UpperCaseLetters = new ReadOnlyCollection<char>(Enumerable
+            .Range('A', 26)
+            .Select(i => (char)i)
+            .ToList());
+    }
+
+    public static string Rot13(string message)
+    {
+        return new String(message
+            .Select(ToRot13)
+            .ToArray());
+    }
+
+    private static char ToRot13(char c)
+    {
+        if (LowerCaseLetters.Contains(c))
+        {
+            int rot13Index = (LowerCaseLetters.IndexOf(c) + 13) % 26;
+            return LowerCaseLetters.ElementAt(rot13Index);
+        }
+
+        if (UpperCaseLetters.Contains(c))
+        {
+            int rot13Index = (UpperCaseLetters.IndexOf(c) + 13) % 26;
+            return UpperCaseLetters.ElementAt(rot13Index);
+        }
+
+        return c;
+    }
+}
