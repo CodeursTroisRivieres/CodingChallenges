ca50df55de3a36c6d34bf02ff70d7c7f5ad5c594
Roger Leblanc <rleblanc@mmbs.ca>
2021-07-10 11:34:28 -0400

    Add LongestWord

---
+++ LongestWord/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LongestWord
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // keep this function call here
+            Console.WriteLine(LongestWord(Console.ReadLine()));
+        }
+
+        public static string LongestWord(string sentence)
+        {
+            string onlyLettersAndNumbers = Regex.Replace(sentence, "[^a-zA-Z0-9 ]", string.Empty);
+
+            return onlyLettersAndNumbers
+                .Split(" ")
+                .GroupBy(x => x.Length)
+                .OrderByDescending(g => g.Key)
+                .Select(g => g.First())
+                .FirstOrDefault();
+        }
+    }
+}
